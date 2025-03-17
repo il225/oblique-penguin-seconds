e1dcddb40f8e6b809d2a8f2cdf2be5b1a930adcf
Isaac Lanning <il225023@ohio.edu>
2025-03-17 18:59:35 -0400

    it should work now

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/its_a_website/",
+  base: "/oblique-penguin-seconds/",
   plugins: [react()],
 })
